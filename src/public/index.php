4543f1ef20dcfacfb0ab5b830c294282b04b541e
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-09-20 22:04:14 -0300

    configuração da sessão e rota padrão

--- src/public/index.php
+++ src/public/index.php
@@ -6,6 +6,8 @@ use App\Controllers\ProductsController;
 use App\Core\Application;
 use App\Core\Router;
 
+session_set_cookie_params(['secure' => true, 'httponly' => true, 'samesite' => 'lax']);
+session_start();
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../docker');
 $dotenv->load();
 
@@ -17,6 +19,9 @@ $router = new Router();
 $router->get('/login', [AuthController::class, 'loginView']);
 $router->post('/login', [AuthController::class, 'checkCredentials']);
 $router->get('/dashboard', [ProductsController::class, 'dashboardView']);
+
+// Rota padrão em caso de URL inválida
+$router->setMainRoute('/login');
 
 $response = $router->dispatch($_SERVER["REQUEST_URI"], $_SERVER["REQUEST_METHOD"]);
 
