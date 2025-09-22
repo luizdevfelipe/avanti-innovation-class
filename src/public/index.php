3562622edd1a503003292d7adcf7a064e8f5a6db
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-09-21 22:09:33 -0300

    Novas rotas
    - Rotas para gerenciamento dos produtos
    - Bloco try catch para minimizar mensagens de erros

--- src/public/index.php
+++ src/public/index.php
@@ -18,11 +18,25 @@ $router = new Router();
 // Rotas principais
 $router->get('/login', [AuthController::class, 'loginView']);
 $router->post('/login', [AuthController::class, 'checkCredentials']);
+$router->post('/logout', [AuthController::class, 'logout']);
+
 $router->get('/dashboard', [ProductsController::class, 'dashboardView']);
+
+$router->post('/products', [ProductsController::class, 'addProduct']);
+$router->get('/products', [ProductsController::class, 'searchProducts']);
+$router->get('/products/{id}', [ProductsController::class, 'getProductData']);
+$router->post('/products/{id}/delete', [ProductsController::class, 'deleteProduct']);
+$router->post('/products/{id}/edit', [ProductsController::class, 'editProduct']);
 
 // Rota padrão em caso de URL inválida
 $router->setMainRoute('/login');
 
-$response = $router->dispatch($_SERVER["REQUEST_URI"], $_SERVER["REQUEST_METHOD"]);
+try {
+    $response = $router->dispatch($_SERVER["REQUEST_URI"], $_SERVER["REQUEST_METHOD"]);
+} catch (Exception $e) {
+    http_response_code(500);
+    echo "Erro interno do servidor: " . $e->getMessage();
+    exit;
+}
 
 echo $response ?? 'No response found for the route.';
